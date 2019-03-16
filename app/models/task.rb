6086a2493d5f366d202af14b75459fe6f416dea1
bmulla <bmulla@qatar.cmu.edu>
2019-03-16 19:32:21 +0000

    all tests pass

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,7 @@ class Task < ApplicationRecord
     validates :name, presence: true
     validates :points, numericality: {greater_than_or_equal_to: 1}
     validates :points, numericality: { only_integer: true }
+    
+    scope :alphabetical, -> { order('name ASC') }
+    scope :active, -> { where('active = ?', true) }
 end
