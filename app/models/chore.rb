731c1ba3c9e62bf6180367db06ead819f29cc6fc
bmulla <bmulla@qatar.cmu.edu>
2019-03-16 20:18:48 +0000

    scopes: by_task and chronolifical have successful tests

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,4 +2,7 @@ class Chore < ApplicationRecord
     belongs_to :child
     belongs_to :task
     validates_datetime :due_on
+    
+    scope :by_task, -> { order('task_id DESC') }
+    scope :chronological, -> { order('due_on ASC, completed DESC') }
 end
